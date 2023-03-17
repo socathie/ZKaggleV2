7d52b98c9ee1f0020e625bf0a21a707b005ab7be
drCathieSo.eth <socathie@users.noreply.github.com>
2023-03-18 04:07:32 +0800

    feat: latest rainbowkit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,49 +2,37 @@ import './App.css'
 
 import '@rainbow-me/rainbowkit/styles.css';
 import {
-  connectorsForWallets,
-  wallet,
+  getDefaultWallets,
   RainbowKitProvider,
-  midnightTheme,
-} from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { localhost } from 'wagmi/chains';
+// import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 import { YourApp } from './YourApp';
 
 const { chains, provider } = configureChains(
+  [localhost],
   [
-    chain.polygon,
-    chain.localhost,
-  ],
-  [
-    alchemyProvider({ apiKey: import.meta.env.ALCHEMY_ID }),
-    publicProvider(),
+    // alchemyProvider({ apiKey: import.meta.env.ALCHEMY_ID }),
+    publicProvider()
   ]
 );
-
-const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended",
-    wallets: [
-      wallet.metaMask({ chains, shimDisconnect: true }),
-      wallet.walletConnect({ chains }),
-      wallet.rainbow({ chains }),
-    ],
-  },
-]);
-
-const wagmiClient = createClient({
-  autoConnect: false,
-  connectors,
-  provider,
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
 });
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 const App = () => {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={midnightTheme()} coolMode>
+      <RainbowKitProvider chains={chains}>
         <YourApp />
       </RainbowKitProvider>
     </WagmiConfig>
